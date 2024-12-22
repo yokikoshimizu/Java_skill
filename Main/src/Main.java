90e720627f935e8e8ae26c8676084b66944ee551
yokikoshimizu <yokikoshimizu@gmail.com>
2024-12-22 13:08:34 +0900

    throws IOException　を削除しました！演算子の両サイドに半角スペースを入れました！

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,10 +1,10 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException 
+    public static void main(String[] args)
     {
     	int x = 7;
-    	System.out.println(x*3);
-    	System.out.println((double)x/2);
+    	System.out.println(x * 3);
+    	System.out.println((double)x / 2);
     }
 }
