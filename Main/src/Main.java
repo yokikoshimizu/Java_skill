117ffd31cd38180935d08280b4c0e3b8d77ab090
yokikoshimizu <yokikoshimizu@gmail.com>
2024-12-22 09:32:18 +0900

    change

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -3,6 +3,17 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException 
     {
-    	System.out.println("Hello World");
+    	int x = 3;
+    	int y = 5;
+    	
+    	System.out.println("入れ替え前: x = " + x + "y = " + y);
+    	
+    	int temp = x;
+    	x = y;
+    	y = temp;
+    	
+    	//xの値をtempに一時保存⇒yにtemp(=x)を後で代入する手順
+    	
+    	System.out.println("入れ替え後: x = " + y + "y = " + x);
     }
 }
